d69b1e01cdb3d5ca6e3bf716df0c0d6e6fefd086
Frank Urbach <frank.urbach@gmx.de>
2020-12-20 22:41:10 +0100

    Zwischenstand

--- src/SearchLightOracle.jl
+++ src/SearchLightOracle.jl
@@ -1,5 +1,47 @@
 module SearchLightOracle
 
-# Write your package code here.
+import DataFrames, Logging
+import Oracle
+
+import SearchLight
+
+import SearchLight: storableFields, fields_to_store_directly
+
+const DEFAULT_PORT = 1521
+
+const COLUMN_NAME_FIELD_NAME = :column_name
+
+function SearchLight.column_field_name()
+  COLUMN_NAME_FIELD_NAME
+end
+
+const DatabaseHandle = Oracle.Connection
+const ResultHandle   = Oracle.Result
+
+#
+# Connection
+#
+
+
+"""
+    connect(conn_data::Dict)::DatabaseHandle
+
+Connects to the database and returns a handle.
+"""
+function SearchLight.connect(conn_data::Dict = SearchLight.config.db_config_settings) :: DatabaseHandle
+  dns = String[]
+
+  haskey(conn_data, "host")     && push!(dns, string("host=", conn_data["host"]))
+  haskey(conn_data, "hostaddr") && push!(dns, string("hostaddr=", conn_data["hostaddr"]))
+  haskey(conn_data, "port")     && push!(dns, string("port=", conn_data["port"]))
+  haskey(conn_data, "database") && push!(dns, string("dbname=", conn_data["database"]))
+  haskey(conn_data, "username") && push!(dns, string("user=", conn_data["username"]))
+  haskey(conn_data, "password") && push!(dns, string("password=", conn_data["password"]))
+  haskey(conn_data, "passfile") && push!(dns, string("passfile=", conn_data["passfile"]))
+  haskey(conn_data, "connect_timeout") && push!(dns, string("connect_timeout=", conn_data["connect_timeout"]))
+  haskey(conn_data, "client_encoding") && push!(dns, string("client_encoding=", conn_data["client_encoding"]))
+
+  push!(CONNECTIONS, LibPQ.Connection(join(dns, " ")))[end]
+end
 
 end
---
+++ test/test_oracle.jl
@@ -0,0 +1,45 @@
+using Test, TestSetExtensions, SafeTestsets
+
+include(joinpath(@__DIR__,"test_models.jl"))
+module TestSetupTeardown
+
+  using SearchLight
+  using SearchLightOracle
+
+  export prepareDbConnection, tearDown
+
+  connection_file = "oracle_connection.yml"
+
+  function prepareDbConnection()
+      
+    conn_info_oracle = SearchLight.Configuration.load(connection_file)
+    conn = SearchLight.connect(conn_info_oracle)
+    return conn
+  end
+
+  function tearDown(conn)
+    if conn !== nothing
+        ######## Dropping used tables
+        SearchLight.Migration.drop_migrations_table()
+
+        # insert tables you use in tests here
+        tables = ["Book","BookWithIntern","Callback","Author","BookWithAuthor"]
+
+        # obtain tables exists or not, if they does drop it
+        wheres = join(map(x -> string("'", lowercase(SearchLight.Inflector.to_plural(x)), "'"), tables), " , ", " , ")
+        queryString = string("select table_name from information_schema.tables where table_name in ($wheres)")
+        result = SearchLight.query(queryString)
+        for item in eachrow(result)
+            try
+                SearchLight.Migration.drop_table(lowercase(item[1]))
+            catch ex
+                @show "Table $item doesn't exist"
+            end 
+        end 
+  
+        SearchLight.disconnect(conn)
+        rm(SearchLight.config.db_migrations_folder, force=true, recursive=true)
+    end
+  end
+
+end
